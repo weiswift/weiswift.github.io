318daae59d46638690ac404753febc024f79746e
e <1265019024@qq.com>
2020-11-01 17:31:47 +0800

    Site updated: 2020-11-01 16:31:46

--- pacman/index.js
+++ pacman/index.js
@@ -1,4 +1,4 @@
-<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script><script class="meting-secondary-script-marker" src="\assets\js\Meting.min.js"></script>//主程序,业务逻辑
+//主程序,业务逻辑
 (function(){
 	var _COIGIG = [		//关卡
 		{				//第1关
