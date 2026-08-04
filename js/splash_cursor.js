4dbc761bdeed343fad16db4d383ece60efe462ce
weiswift <weiswift@users.noreply.github.com>
2026-08-04 10:37:55 +0800

    Site updated: 2026-08-04 10:37:54

--- js/splash_cursor.js
+++ js/splash_cursor.js
@@ -45,7 +45,7 @@
 
   // Create canvas container
   const container = document.createElement('div');
-  container.style.cssText = 'position:fixed;top:0;left:0;z-index:-1;pointer-events:none;width:100%;height:100%';
+  container.style.cssText = 'position:fixed;top:0;left:0;z-index:2147483647;pointer-events:none;width:100%;height:100%';
 
   const canvas = document.createElement('canvas');
   canvas.id = 'fluid';
